e3767736d76ee9de10b50cf178ab573ab131bae8
sidheshwarchoure <92964594+sidheshwarchoure@users.noreply.github.com>
2023-03-22 10:26:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -23,6 +23,26 @@ class Teacher extends Person {
 		console.log("this.name"+"is teaching");
 	}
 }
+// create a Person object
+const p = new Person("Alice", 25);
+
+// get the name using the getter method
+console.log(p.name); // Alice
+
+// set the age using the setter method
+p.age = 30;
+
+// create a Student object
+const s = new Student("Bob", 20);
+
+// call the study method
+s.study(); // Bob is studying
+
+// create a Teacher object
+const t = new Teacher("Carol", 35);
+
+// call the teach method
+t.teach(); // Carol is teaching
 
 // Do not change the code below this line
 window.Person = Person;
