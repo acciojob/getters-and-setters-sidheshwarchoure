135d1baa18fde6d8d6109220ae95de52f71e20a6
sidheshwarchoure <92964594+sidheshwarchoure@users.noreply.github.com>
2023-03-22 10:41:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,47 +2,27 @@
 class Person {
 	constructor(name,age){
 		this.name = name;
-		this.age = age;c
+		this.age = age;
 	}
 }
 get name(){
 	return this.name;
 }
-set age(){
+set age(age){
 	 this.age=age;
 }
 
 class Student extends Person {
-	name(){
+	study(){
 		console.log(this.name + "is studying");
 	}
 }
 
 class Teacher extends Person {
 	teach(){
-		console.log("this.name"+"is teaching");
+		console.log(this.name+"is teaching");
 	}
 }
-// create a Person object
-const p = new Person("Alice", 25);
-
-// get the name using the getter method
-console.log(p.name); // Alice
-
-// set the age using the setter method
-p.age = 30;
-
-// create a Student object
-const s = new Student("Bob", 20);
-
-// call the study method
-s.study(); // Bob is studying
-
-// create a Teacher object
-const t = new Teacher("Carol", 35);
-
-// call the teach method
-t.teach(); // Carol is teaching
 
 // Do not change the code below this line
 window.Person = Person;
